87d9157b445f8db78f6b6b9ebb8a8207d3e32b17
Eugene Glushchuk <e.glushuk@idaproject.com>
2021-06-13 12:08:09 +0300

    feat(auth.routes)

--- app.ts
+++ app.ts
@@ -1,8 +1,18 @@
 import express, {Application, Request, Response, NextFunction} from 'express';
 import config, {IConfig} from 'config';
+import mongoose from 'mongoose';
 
 const app: Application = express();
-
 const PORT: IConfig = config.get('port') || 5000;
 
-app.listen(PORT, () => console.log('running'));
+app.use('/api/auth', require('./routes/auth.routes'));
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'));
+        app.listen(PORT, () => console.log(`App has benn started on port: ${PORT}`));
+    } catch (e) {
+        console.log('Server error', e.message);
+        process.exit(1);
+    }
+}
